436891082053352fef7f4f800f4e39771dd07c99
Pavla Hrabcová <pavla.hrabcova@avast.com>
2022-09-29 13:42:30 +0200

    updates of avast end northon employees

--- Avast_employee.py
+++ Avast_employee.py
@@ -3,10 +3,23 @@ from class_employee import Employee
 
 class AvastEmployee(Employee):
     employer = 'Avast'
+    message_tool = 'Slack'
+
+    @classmethod
+    def change_employer_to_northon(cls):
+        cls.employer = 'Northon'
 
     def __str__(self):
         return f"{self.name} - '{type(self).employer}'"
 
+Vendula = AvastEmployee('Vendula', 'Hrabcova', 10, True)
+print(Vendula)
+print(Vendula.increase_salary_percentage(50))
+print(Vendula.get_number_of_employees())
+print(Vendula.change_employer_to_northon())
+print(Vendula)
+Vendula.fire_employee()
+print(Vendula.active)
 
 
 #vyrobit obdobnou třídu northnon employee a obohatit to nějak a rozkošatet to
--- Northon_employee.py
+++ Northon_employee.py
@@ -3,7 +3,17 @@ from class_employee import Employee
 
 class NorthonEmployee(Employee):
     employer = 'Northon'
+    message_tool = 'MS Teams'
+    country = 'India'
+
+    @classmethod
+    def country_of_origin(cls, country):
+        cls.country = country
 
     def __str__(self):
         return f"{self.name} - '{type(self).employer}'"
 
+Ludmila = NorthonEmployee('Ludmila', 'Hrabcova', 20, True)
+print(Ludmila)
+Ludmila.country_of_origin('Czechia')
+print(Ludmila.country)
